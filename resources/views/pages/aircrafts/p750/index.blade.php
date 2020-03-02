b7899ed30ceff3d3b464cdb77f38ea6ac91124c3
Pasha <pasha@unaik.com>
2020-03-02 17:01:29 +0200

    [update] Some update and test

--- resources/views/pages/aircrafts/p750/index.blade.php
+++ resources/views/pages/aircrafts/p750/index.blade.php
@@ -8,7 +8,7 @@
     <div class="page-header p750-background header-filter dark-filter">
         <div class="container">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <h1 class="title text-blue-gradient">
                         P750 - XSTOL
                     </h1>
@@ -25,7 +25,7 @@
                         </a>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6">
                     <iframe width="100%" src="https://www.youtube.com/embed/YF9egPPC3kI" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
